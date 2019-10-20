ee9bf3b9248bd448daec5350610eac74c12dc74f
Mayank Shrivastava <mayanksri910@gmail.com>
2019-10-20 19:48:57 +0530

    Update Fibonacci.java

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -22,6 +22,19 @@ public class Fibonacci {
         System.out.println(fibMemo(size));
         System.out.println(fibBotUp(size));
     }
+    public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
 
     /**
      * This method finds the nth fibonacci number using memoization technique
